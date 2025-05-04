e9dd85b64fb9301ae05cf6c9136df4527acba797
Rayhan Al Kavey <91812273+RayhanalKavey@users.noreply.github.com>
2025-05-04 10:07:56 +0600

    Add user table, and create other table's file.

---
+++ database/migrations/2025_05_04_034519_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('user_id'); //column type
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnUpdate()->restrictOnDelete();
+            $table->timestamps();
+            /* 
+             * cascadeOnUpdate(): If a user's id were ever updated (uncommon, but possible), the corresponding user_id in the projects table would update automatically.
+             * restrictOnDelete(): This prevents a user from being deleted if they still have projects associated with them. The database will throw an error if you try. This is often a good safety measure. */
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
